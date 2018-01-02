e4b7912ea1dbc2dbe89ade0eb8fe29e18b794766
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-02 14:49:24 -0800

    Fix user model tests

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,9 +1,23 @@
 require "test_helper"
 
-# describe Message do
-#   let(:message) { Message.new }
-#
-#   it "must be valid" do
-#     value(message).must_be :valid?
-#   end
-# end
+describe Message do
+  describe "validations" do
+    # it "allows a message to be created with all required parameters" do
+    #   start_count = Message.all.count
+    #
+    #   message_data = {
+    #     text: "hello world",
+    #     from: 1,
+    #     to: 2,
+    #     language: "ru"
+    #   }
+    #
+    #   message = Message.new(message_data)
+    #   end_count = Message.all.count
+    #
+    #   message.must_be :valid?
+    #   end_count.must_equal start_count + 1
+    #
+    # end
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,26 +3,35 @@ require "test_helper"
 describe User do
   describe "validations" do
     it "allows a user to be created with all required data" do
+      start_count = User.all.count
+
       user_data = {
         username: "New User",
-        language: "ru" }
+        language: "ru"
+      }
 
       user = User.new(user_data)
+      user.save
+      end_count = User.all.count
 
       user.must_be :valid?
+      end_count.must_equal start_count + 1
     end
 
     it "requires all the data to create a new user" do
+      start_count = User.all.count
       invalid_user_data = {
         username: ""
       }
       user = User.new(invalid_user_data)
+      end_count = User.all.count
 
       user.wont_be :valid?
+      end_count.must_equal start_count
     end
 
     it "requires unique username to create a new user" do
-      start_user_count = User.all.length
+      start_count = User.all.count
 
       user_data = {
         username: "New User",
@@ -44,9 +53,9 @@ describe User do
       new_user = User.new(valid_user_data)
       new_user.save
 
-    end_user_count = User.all.length
+    end_count = User.all.count
 
-    end_user_count.must_equal start_user_count + 2
+    end_count.must_equal start_count + 2
     end
   end
 end
