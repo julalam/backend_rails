69c70c0c56a4d0cec9f3029c321fdbd4bde4a57c
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-04 16:06:57 -0800

    Add relationships to the models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
+  has_one :language
+  has_one :from, :class_name => "User"
+  has_one :to, :class_name => "User"
+
   validates :text, presence: true
   validates :from, presence: true
   validates :to, presence: true
