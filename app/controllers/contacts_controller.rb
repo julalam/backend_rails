0635607b74287c2f81b9bdf6e47eecfc5333e096
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-09 11:48:40 -0800

    Add contacts controller and tested methods in postman

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,49 @@
+class ContactsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    contacts = []
+    result = Contact.where(from: params[:from], status: 'accepted')
+    result.each do |contact|
+      contacts << contact
+    end
+    result = Contact.where(to: params[:from], status: 'accepted')
+    result.each do |contact|
+      contacts << contact
+    end
+    requests = []
+    result = Contact.where(to: params[:from], status: 'pending')
+    result.each do |contact|
+      requests << contact
+    end
+
+    render status: :ok, json: {contacts: contacts, requests: requests}
+  end
+
+  def create
+    contact = Contact.new(contact_params)
+
+    if contact.save
+      render status: :ok, json: contact
+    else
+      render status: :bad_request, json: {errors: contact.errors.messages}
+    end
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    contact.status = params[:status]
+
+    if contact.save
+      render status: :ok, json: contact
+    else
+      render status: :bad_request, json: {errors: contact.errors.messages}
+    end 
+  end
+
+  private
+
+  def contact_params
+    params.permit(:from, :to)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only:[:index, :create]
   resources :messages, only:[:index, :create]
   resources :languages, only:[:index]
+  resources :contacts, only:[:index, :create, :update]
 
   # custom routes
   post 'login', to: 'users#login'
