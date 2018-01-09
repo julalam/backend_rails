8b585f1cd5ab7e9c7f3ca82684b5b9080de14ce3
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-09 14:46:17 -0800

    Fix relations between user and contact models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
-  belongs_to :from, :class_name => 'User', :foreign_key => 'id'
-  belongs_to :to, :class_name => 'User', :foreign_key => 'id'
+  belongs_to :from_user, :class_name => 'User', :foreign_key => 'from'
+  belongs_to :to_user, :class_name => 'User', :foreign_key => 'to'
 
   validates :from, presence: true
   validates :to, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :from_contacts, :class_name => 'Contact', :foreign_key => 'id'
-  has_many :to_contacts, :class_name => 'Contact', :foreign_key => 'id'
+  has_many :from_contacts, :class_name => 'Contact', :foreign_key => 'from'
+  has_many :to_contacts, :class_name => 'Contact', :foreign_key => 'to'
 
   validates :username, presence: true, uniqueness: true
   validates :language, presence: true
