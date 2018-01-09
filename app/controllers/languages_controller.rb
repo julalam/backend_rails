7ea2cf3a2720fa9e6d5d840f8adf8c656c3d05c4
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-08 16:16:30 -0800

    Add seed data for languages

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,21 +1,5 @@
-require 'csv'
-
 class LanguagesController < ApplicationController
   def index
-
-  file = Rails.root.join('lib', 'languages.csv')
-
-  language_failures = []
-  CSV.foreach(file, :headers => true) do |row|
-    language = Language.new
-    language.id = row['id']
-    language.name = row['name']
-    language.code = row['code']
-    if !language.save
-      language_failures << language
-    end
-  end
-
     languages = Language.all
 
     render status: :ok, json: languages
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+FILE = Rails.root.join('db', 'languages.csv')
+
+language_failures = []
+CSV.foreach(FILE, :headers => true) do |row|
+  language = Language.new
+  language.id = row['id']
+  language.name = row['name']
+  language.code = row['code']
+  if !language.save
+    language_failures << language
+  end
+end
