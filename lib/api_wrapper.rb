766a4897ce6fb12c5b3da73c573b83ebdb85e00f
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-08 18:47:50 -0800

    Fix api wrapper url

--- lib/api_wrapper.rb
+++ lib/api_wrapper.rb
@@ -6,25 +6,25 @@ class APIWrapper
   GOOGLE_CREDENTIALS = ENV["GOOGLE_CREDENTIALS"]
   GOOGLE_KEY = ENV["GOOGLE_KEY"]
 
-  YANDEX_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
+  YANDEX_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="
   YANDEX_KEY = ENV["YANDEX_KEY"]
 
   def self.translate(text, lang)
-    google_url = GOOGLE_URL + GOOGLE_CREDENTIALS + "&key=" + GOOGLE_KEY + "&g=" + text + "&target=" + lang
+    google_url = GOOGLE_URL + GOOGLE_CREDENTIALS + "&key=" + GOOGLE_KEY + "&q=" + text + "&target=" + lang
 
     response = HTTParty.post(google_url)
-    if response.code == 200
+    if response["data"]
       return response["data"]["translations"].first["translatedText"]
     end
 
-    yandex_url = YANDEX_URL + "key=" + YANDEX_KEY + "&text=" + text + "&lang=" + lang
+    yandex_url = YANDEX_URL + YANDEX_KEY + "&text=" + text + "&lang=" + lang
 
     response =  HTTParty.post(yandex_url)
-    if response.code == 200
+    if response["code"] == 200
       return response["text"].first
-    else
-      return nil
     end
+
+    return nil
   end
 
 end
