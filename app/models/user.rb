599274e634b603f75c42204302af976f3ab78c86
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-11 19:24:19 -0800

    Add regex validation for users email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :from_contacts, :class_name => 'Contact', :foreign_key => 'from'
   has_many :to_contacts, :class_name => 'Contact', :foreign_key => 'to'
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
   validates :language, presence: true
+  validates :email, presence: true, format: { with: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i }
+  validates :password, presence: true, length: {in: 6..10 }
 end
