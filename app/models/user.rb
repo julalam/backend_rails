b2d9358213164e0da61399cf974be0d3eb1dd9fe
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-15 23:24:13 -0800

    Change user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :from_contacts, :class_name => 'Contact', :foreign_key => 'from'
   has_many :to_contacts, :class_name => 'Contact', :foreign_key => 'to'
 
-  validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..15 }
   validates :language, presence: true
   validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
-  validates :password, presence: true, length: {in: 6..10 }
+  validates :password, presence: true, length: { in: 6..10 }
 end
