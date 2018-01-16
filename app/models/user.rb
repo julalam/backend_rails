b25749aa9c46b0bf7a488b189142333076a2b864
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-15 17:25:26 -0800

    Add USA to list of countries

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
   validates :language, presence: true
-  # validates :email, presence: true, format: { with: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i }
+  validates :email, presence: true, format: { with: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i }
   validates :password, presence: true, length: {in: 6..10 }
 end
