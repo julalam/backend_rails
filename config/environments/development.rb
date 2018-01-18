e64e91e0cde77447b0b5cdb16024711f1d5da018
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-17 22:25:43 -0800

    Change config paperclip file to get the correct avatar url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,13 +54,12 @@ Rails.application.configure do
   #upload images
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: 's3-us-west-2.amazonaws.com',
     s3_credentials: {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     },
     s3_region: ENV['AWS_REGION'],
-    bucket: ENV['AWS_BUCKET'],
-    url: ':s3_domain_url',
-    path: "/:class/:attachment/:id_partition/:style/:filename"
+    bucket: ENV['AWS_BUCKET']
   }
 end
