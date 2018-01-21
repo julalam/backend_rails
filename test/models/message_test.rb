4a1baaecac4c89d94c832db3dacda9db14ac3eb8
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-21 09:47:22 -0800

    Add tests for model message

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -3,38 +3,38 @@ require "test_helper"
 describe Message do
   describe "relations" do
     before do
-      @message = Message.last
+      @message = messages(:one)
     end
 
-    # it "a message has one from_user" do
-    #   @message.must_respond_to :from_user
-    #   @message.from_user.must_be_kind_of User
-    # end
-    #
-    # it "a message has one to_user" do
-    #   @message.must_respond_to :to_user
-    #   @message.to_user.must_be_kind_of User
-    # end
+    it "a message has one from_user" do
+      @message.must_respond_to :from_user
+      @message.from_user.must_be_kind_of User
+    end
+
+    it "a message has one to_user" do
+      @message.must_respond_to :to_user
+      @message.to_user.must_be_kind_of User
+    end
   end
 
   describe "validations" do
-  #   it "allows a message to be created with all required parameters" do
-  #     start_count = Message.all.count
-  #
-  #     message_data = {
-  #       text: "hello world",
-  #       from: 9,
-  #       to: 10,
-  #       language: "ru"
-  #     }
-  #
-  #     message = Message.new(message_data)
-  #     message.save
-  #     end_count = Message.all.count
-  #
-  #     message.must_be :valid?
-  #     end_count.must_equal start_count + 1
-  #   end
+    it "allows a message to be created with all required parameters" do
+      start_count = Message.all.count
+
+      message_data = {
+        text: "hello world",
+        from: 1,
+        to: 2,
+        language: "ru"
+      }
+
+      message = Message.new(message_data)
+      message.save
+      end_count = Message.all.count
+
+      message.must_be :valid?
+      end_count.must_equal start_count + 1
+    end
 
     it "requires all required data to create a new message" do
       start_count = Message.all.count
