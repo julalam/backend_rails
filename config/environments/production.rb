d80480e81fb5b7f43e0a3a6840f58f20e4fb42fa
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-23 12:28:15 -0800

    Add action cable to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'ws://example-env.mpmpcu8qtu.us-west-2.elasticbeanstalk.com/cable'
+  config.action_cable.allowed_request_origins = '*'
 end
