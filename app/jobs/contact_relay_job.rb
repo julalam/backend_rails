b6c80397bfafbc17958157966522aa99769faa47
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-24 14:20:10 -0800

    Add language to contact job

--- app/jobs/contact_relay_job.rb
+++ app/jobs/contact_relay_job.rb
@@ -1,7 +1,7 @@
 class ContactRelayJob < ApplicationJob
   def perform(contact)
 
-    ActionCable.server.broadcast "contacts_#{contact.to}", user: contact.from_user, avatar: contact.from_user.avatar.url, status: contact.status == 'pending' ? 'received_request' : 'friend', contact: contact.status == 'pending' ? contact : nil
-    ActionCable.server.broadcast "contacts_#{contact.from}", user: contact.to_user, avatar: contact.to_user.avatar.url, status: contact.status == 'pending' ? 'sent_request' : 'friend', contact: contact.status == 'pending' ? contact : nil
+    ActionCable.server.broadcast "contacts_#{contact.to}", user: contact.from_user, avatar: contact.from_user.avatar.url, language: contact.from_user.language.name, status: contact.status == 'pending' ? 'received_request' : 'friend', contact: contact.status == 'pending' ? contact : nil
+    ActionCable.server.broadcast "contacts_#{contact.from}", user: contact.to_user, avatar: contact.to_user.avatar.url, language: contact.to_user.language.name, status: contact.status == 'pending' ? 'sent_request' : 'friend', contact: contact.status == 'pending' ? contact : nil
   end
 end
