838187b71c3e3261e53e6652b07b1aba8227953f
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-23 19:43:35 -0800

    Remove localhost from origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.action_cable.url = 'ws://speakeasy-rails.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['http://localhost:3000', 'http://example-env.mpmpcu8qtu.us-west-2.elasticbeanstalk.com']
+  config.action_cable.allowed_request_origins = ['http://example-env.mpmpcu8qtu.us-west-2.elasticbeanstalk.com']
 
   config.action_cable.url = 'ws://example-env.mpmpcu8qtu.us-west-2.elasticbeanstalk.com/cable'
   # config.action_cable.allowed_request_origins = ['http://localhost:3000']
